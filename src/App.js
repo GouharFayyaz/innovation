d42ec0e062c9632665fea3e1c0a486bfec83b422
Gouhar Fayyaz <gfayyaz4@gmail.com>
2023-10-26 22:53:54 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Chi from "./components/chisiamo";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import Nav from "./components/nav";
+import Qout from "./components/qouts";
+
+
+function App() {
+  return (
+    <div>
+
+      <Nav/>
+      <Hero/>
+      <Chi/>
+      <Qout/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
